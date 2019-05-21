bc723bed9a91d571bb464865fd208fd6e3858ae7
miraae <yes_future@naver.com>
2019-05-21 16:20:12 +0900

    initialEvaluation Activity(xml,java)추가 Login 이후 initialEvaluation activity 자동 실행되도록함

---
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/initialEvaluation.java
@@ -0,0 +1,14 @@
+package com.skkuseteam2.eatit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class initialEvaluation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_evaluation);
+    }
+}
