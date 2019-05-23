d61fa66fbab41ccb528e630c98d47f62d5edba2f
miraae <yes_future@naver.com>
2019-05-23 13:18:37 +0900

    myPage, orderHistory 파일 생성

---
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/myPage.java
@@ -0,0 +1,13 @@
+package com.skkuseteam2.eatit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class myPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
