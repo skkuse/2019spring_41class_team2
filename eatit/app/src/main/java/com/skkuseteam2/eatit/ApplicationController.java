bb6517c756f915bc36b3251499b7ddb9c22f6be4
miraae <yes_future@naver.com>
2019-05-28 18:13:35 +0900

    android: 통신을 위한 api 추가, build.gradle, androidManifast 수정

---
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/ApplicationController.java
@@ -0,0 +1,9 @@
+package com.skkuseteam2.eatit;
+
+import android.app.Application;
+import android.util.Log;
+import retrofit2.Retrofit;
+
+public class ApplicationController extends Application {
+
+}
