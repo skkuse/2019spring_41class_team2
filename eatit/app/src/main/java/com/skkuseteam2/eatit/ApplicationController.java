31f26617a5f49c08c7b0e4be707c16333541aff4
miraae <yes_future@naver.com>
2019-05-29 19:16:01 +0900

    bottom navigation bar 구현 - fragment별로 switch

--- eatit/app/src/main/java/com/skkuseteam2/eatit/ApplicationController.java
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/ApplicationController.java
@@ -1,9 +1,56 @@
+// 서버와 통신하기 위해 instance를 생성하고 받아온 port, ip를 설정하는 클래스
 package com.skkuseteam2.eatit;
 
 import android.app.Application;
 import android.util.Log;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApplicationController extends Application {
 
+    public final static String TAG = "NMR";
+    private static ApplicationController instance;
+    public static ApplicationController getInstance() {return instance;}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ApplicationController.instance = this; // 처음 실행될때 인스턴트 생성
+    }
+
+    private NetworkService networkService;
+    public NetworkService getNetworkService() {return networkService;}
+
+    private String baseUrl;
+
+    public void buildNetworkService(String ip, int port) {
+        synchronized (ApplicationController.class) {
+            if (networkService == null) {
+                baseUrl = String.format("http://%s:%d/", ip, port);
+                Log.i(TAG, baseUrl);
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(baseUrl)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                networkService = retrofit.create(NetworkService.class);
+            }
+        }
+    }
+
+    public void buildNetworkService(String ip) {
+        synchronized (ApplicationController.class) {
+            if (networkService == null) {
+                baseUrl = String.format("http://%s/", ip);
+                Log.i(TAG, baseUrl);
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(baseUrl)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                networkService = retrofit.create(NetworkService.class);
+            }
+        }
+    }
 }
--- eatit/app/src/main/java/com/skkuseteam2/eatit/MainActivity.java
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/MainActivity.java
@@ -1,40 +1,45 @@
 package com.skkuseteam2.eatit;
 
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.StateListAnimator;
-import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.nhn.android.naverlogin.OAuthLogin;
-import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
-
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+
+    // frame layout에 각 메뉴의 fragment 표시
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    // 각 fragment들
+    private HomeFragment homeFragment = new HomeFragment();
+    private SearchFragment searchFragment = new SearchFragment();
+    private MyPageFragment myPageFragment = new MyPageFragment();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
+                    transaction.replace(R.id.contents, homeFragment).commitAllowingStateLoss();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    transaction.replace(R.id.contents, searchFragment).commitAllowingStateLoss();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //mTextMessage.setText(R.string.title_notifications);
+                    transaction.replace(R.id.contents, myPageFragment).commitAllowingStateLoss();
                     return true;
             }
             return false;
@@ -53,9 +58,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // LinearLayout contents = findViewById(R.id.contents);
 
+        // set initial page
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.contents, homeFragment).commitAllowingStateLoss();
+
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @Override
