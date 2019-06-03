baa0f0e4d3d03f0f8eb3749725da59188d6dc138
leejb <dlwoqhd1222@gmail.com>
2019-06-03 18:35:25 +0900

    mypage, orderhistory

--- eatit/app/src/main/java/com/skkuseteam2/eatit/orderHistory.java
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/orderHistory.java
@@ -2,6 +2,8 @@ package com.skkuseteam2.eatit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class orderHistory extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class orderHistory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_history);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
     }
 }
--- eatit/app/src/main/java/com/skkuseteam2/eatit/orderViewActivity.java
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/orderViewActivity.java
@@ -5,7 +5,6 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.content.Intent;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -15,33 +14,21 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class myPage extends AppCompatActivity {
+public class orderViewActivity extends AppCompatActivity {
 
     ImageView imageView;
     Bitmap bitmap;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_page);
+        setContentView(R.layout.activity_order_view);
 
-        Button button1 = (Button) findViewById(R.id.button);
-        button1.setOnClickListener(new View.OnClickListener()
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                Intent intent = new Intent(getApplicationContext(), orderHistory.class);
-                startActivity(intent);
-            }
-        });
-
-        Button button2 = (Button) findViewById(R.id.button);
-        button2.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(getApplicationContext(), orderHistory.class); //수정요망
-                startActivity(intent);
+                finish();
             }
         });
 
