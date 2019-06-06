515c45989156aec06268b27bafd618fd86005ae7
장호 <cnadkr@users.noreply.github.com>
2019-06-06 22:32:16 +0900

    주문 페이지 OrderPage 생성 "이름, 연락처, 배송지 입력".

---
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/OrderPage.java
@@ -0,0 +1,21 @@
+package com.skkuseteam2.eatit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class OrderPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_page);
+    }
+}
