3a81a3b59e20874cb67c5399096bab0fff0e213f
miraae <yes_future@naver.com>
2019-06-07 03:22:18 +0900

    안드로이드: servertestfragment: python 서버에 tcp로 메시지 전송 구현

--- eatit/app/src/main/java/com/skkuseteam2/eatit/ServerTestFragment.java
+++ eatit/app/src/main/java/com/skkuseteam2/eatit/ServerTestFragment.java
@@ -1,6 +1,9 @@
 package com.skkuseteam2.eatit;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -14,9 +17,24 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ServerTestFragment extends android.support.v4.app.Fragment {
 
     private NetworkService networkService;
+
+    // tcp 관련 선언
+    private Socket clntSocket;
+    private BufferedReader sockIn;
+    private PrintWriter sockOut;
+    private int port = 60728;
+    private final String ip = "192.168.219.103";
+    private TCPThread tcpThread;
 
     @Nullable
     @Override
@@ -25,18 +43,20 @@ public class ServerTestFragment extends android.support.v4.app.Fragment {
         // View 생성
         View view = inflater.inflate(R.layout.fragment_server_test, container, false);
 
+
         // ip, port 연결, network 연결
         ApplicationController application = ApplicationController.getInstance();
         application.buildNetworkService("52.78.88.3",8080);
         networkService = ApplicationController.getInstance().getNetworkService();
 
+        // db 연동 체크
         Button regButton = (Button)view.findViewById(R.id.regBtn);
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Version version = new Version();
-                version.setVersion("0.0.2.6");
+                version.setVersion("0.0.2.9");
                 Call<Version> testCall = networkService.post_version(version);
                 testCall.enqueue(new Callback<Version>() {
                     @Override
@@ -59,7 +79,50 @@ public class ServerTestFragment extends android.support.v4.app.Fragment {
             }
         });
 
+        // TCP 소켓 통신
+        new TCPConnect().run();
+        Button sendButton = (Button)view.findViewById(R.id.sendBtn);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tcpThread = new TCPThread();
+                tcpThread.start();
+            }
+        });
+
         return view;
     }
 
+    class TCPConnect {
+        public void run() {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            try {
+                clntSocket = new Socket(ip, port);
+                sockIn = new BufferedReader(new InputStreamReader(clntSocket.getInputStream()));
+                sockOut = new PrintWriter(clntSocket.getOutputStream(), true);
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class TCPThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    sockOut.print("hello");
+                    // System.out.println("Message sented!");
+                    sockOut.flush();
+                    return;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
